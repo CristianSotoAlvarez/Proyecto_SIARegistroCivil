d27e8cd6581015aad7fbbddfdeac839ca85db128
Cristian Soto <150101902+CristianSotoAlvarez@users.noreply.github.com>
2024-09-08 17:37:34 +0000

    1er Commit Proyecto SIA Avance, creación de clases Persona y Oficina.

---
+++ src/main/java/Oficina.java
@@ -0,0 +1,28 @@
+
+public class Oficina {
+    private String region;
+    private String comuna;
+    private String direccion;
+    private int codigoOficina;
+
+
+    public void setRegion(String region) {this.region = region;}
+
+    public void setComuna(String comuna) {this.comuna = comuna;}
+
+    public void setDireccion(String direccion) {this.direccion = direccion;}
+
+    public void setCodigoOficina(int codigoOficina) { this.codigoOficina = codigoOficina;}
+
+
+    public String getRegion() {return region;}
+
+    public String getComuna() {return comuna;}
+
+    public String getDireccion() {return direccion;} 
+
+    public int getCodigoOficina() { return codigoOficina;}
+
+
+
+}
---
+++ src/main/java/Persona.java
@@ -0,0 +1,86 @@
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.text.ParseException;
+
+
+public class Persona {
+    private String rut;
+    private String nombre;
+    private Date fechaNacimiento;
+    private Date fechaDefuncion;
+    private String lugarNacimiento;
+    private String estadoCivil;
+    private Date fechaMatrimonio; 
+
+    public void setRut(String rut) {this.rut = rut;}
+
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public void setLugarNacimiento(String lugarNacimiento) {this.lugarNacimiento = lugarNacimiento;}
+
+    public void setEstadoCivil(String estadoCivil) {this.estadoCivil = estadoCivil;}
+
+    public void setFechaDefuncion(String fechaDefuncion) {
+
+        SimpleDateFormat tipo = new SimpleDateFormat("dd-MM-yyyy");
+        tipo.setLenient(false);
+        Date fecha = null;
+
+        try{
+            fecha = tipo.parse(fechaDefuncion);
+            Date fechaActual = new Date();
+
+            if(fecha.after(fechaActual)) System.out.println("ERROR: La fecha ingresada no puede ser futura\n-----------------------------------------------");
+            else this.fechaDefuncion = fecha;
+
+        }catch(ParseException e){ System.out.println("ERROR: El formato de la fecha es invalido\n-----------------------------------------------");}  
+    }
+
+    public void setFechaMatrimonio(String fechaMatrimonio) {
+
+        SimpleDateFormat tipo = new SimpleDateFormat("dd-MM-yyyy");
+        tipo.setLenient(false);
+        Date fecha = null;
+
+        try{
+            fecha = tipo.parse(fechaMatrimonio);
+            Date fechaActual = new Date();
+
+            if(fecha.after(fechaActual)) System.out.println("ERROR: La fecha ingresada no puede ser futura\n-----------------------------------------------");
+            else this.fechaMatrimonio = fecha;
+
+        }catch(ParseException e){ System.out.println("ERROR: El formato de la fecha es invalido\n-----------------------------------------------");}  
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento){
+
+        SimpleDateFormat tipo = new SimpleDateFormat("dd-MM-yyyy");
+        tipo.setLenient(false);
+        Date fecha = null;
+
+        try{
+            fecha = tipo.parse(fechaNacimiento);
+            Date fechaActual = new Date();
+
+            if(fecha.after(fechaActual)) System.out.println("ERROR: La fecha ingresada no puede ser futura\n-----------------------------------------------");
+            else this.fechaNacimiento = fecha;
+
+        }catch(ParseException e){ System.out.println("ERROR: El formato de la fecha es invalido\n-----------------------------------------------");}  
+
+    } 
+
+    public String getRut() {return rut;}
+
+    public String getNombre() {return nombre;}
+
+    public Date getFechaNacimiento() {return fechaNacimiento;}
+
+    public String getEstadoCivil() {return estadoCivil;}
+
+    public Date getFechaDefuncion() {return fechaDefuncion;}
+
+    public String getLugarNacimiento() {return lugarNacimiento;}
+
+    public Date getFechaMatrimonio() {return fechaMatrimonio;} 
+}
